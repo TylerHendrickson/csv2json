02eb51f29166779e0d51216b0f0bf6ac4194a393
TylerHendrickson <tyler@divvydose.com>
2021-09-17 11:10:44 -0500

    Stashing changes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"encoding/json"
 	"github.com/integrii/flaggy"
@@ -12,35 +13,62 @@ import (
 // record values are a single row's worth of data, keyed by column names
 type record map[string]string
 
-func main() {
-	var colNames []string
-	var fileName string
+type conversionOptions struct {
+	colNames   []string
+	csvInput   io.Reader
+	jsonOutput io.Writer
+	skipErrors bool
+}
 
-	flaggy.SetVersion("0.1.0")
+func main() {
+	if err := runCli(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func runCli() error {
+	var (
+		colNames   []string
+		fileName   string
+		skipErrors bool
+	)
+	flaggy.SetVersion("0.2.0")
 	flaggy.SetDescription("Restructures CSV into JSON")
 	flaggy.StringSlice(&colNames, "c", "force-columns",
 		"Column names, which must equal the number of CSV fields if given. "+
 			"When set, the first line of CSV data is treated as a data row instead of column names.")
 	flaggy.AddPositionalValue(&fileName, "file", 1, false,
 		"The CSV file to convert. If omitted, input is read from stdin.")
+	flaggy.Bool(&skipErrors, "s", "skip-errors",
+		"Skip CSV lines that cause parsing errors. By default, errors abort conversion completely.")
 	flaggy.Parse()
 
 	fd, err := getCsvFile(fileName)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
-	if err := csv2Json(colNames, fd, os.Stdout); err != nil {
-		log.Fatalln(err)
+	options := conversionOptions{
+		colNames:   colNames,
+		csvInput:   fd,
+		jsonOutput: os.Stdout,
+		skipErrors: skipErrors,
 	}
+
+	if err := csv2Json(options); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // csv2Json converts CSV data from io.Reader to a JSON array and emits the result
 // to io.Writer. When `colNames` is empty, headers are derived from the first
 // line of the CSV file. Returns any errors from reading CSV or encoding JSON.
-func csv2Json(colNames []string, input io.Reader, out io.Writer) error {
-	reader := csv.NewReader(input)
+func csv2Json(options conversionOptions) error {
+	reader := getCsvReader(options.csvInput)
 
+	colNames := options.colNames
 	if len(colNames) == 0 {
 		// Read the first line to get column names
 		firstRow, err := reader.Read()
@@ -55,22 +83,32 @@ func csv2Json(colNames []string, input io.Reader, out io.Writer) error {
 		reader.FieldsPerRecord = len(colNames)
 	}
 
+	numRowsWithErrors := 0
+	defer func() {
+		if options.skipErrors && numRowsWithErrors > 0 {
+			log.Printf("Skipped %d lines (rows) due to parsing errors", numRowsWithErrors)
+		}
+	}()
+
 	allRecords := make([]record, 0)
 	for {
 		rowFields, err := reader.Read()
 		if err != nil {
 			if err == io.EOF {
 				break
+			} else if options.skipErrors {
+				numRowsWithErrors++
+				log.Printf(err.Error())
+				continue
 			}
 			return err
 		}
 
-		//thisRecord := make(record, reader.FieldsPerRecord)
 		thisRecord := fieldsToRecord(&colNames, &rowFields)
 		allRecords = append(allRecords, thisRecord)
 	}
 
-	enc := json.NewEncoder(out)
+	enc := json.NewEncoder(options.jsonOutput)
 	if err := enc.Encode(allRecords); err != nil {
 		return err
 	}
@@ -87,6 +125,24 @@ func getCsvFile(fileName string) (*os.File, error) {
 	} else {
 		return os.Open(fileName)
 	}
+}
+
+// getCsvReader prepares the given io.Reader and returns a new *csv.Reader for parsing its contents as a CSV.
+func getCsvReader(r io.Reader) *csv.Reader {
+	// Skip the first rune if it is a BOM
+	br := bufio.NewReader(r)
+	firstRune, _, err := br.ReadRune()
+	if  err != nil {
+		if err != io.EOF {
+			log.Fatal(err)
+		}
+	}
+	if  firstRune != '\uFEFF' {
+		// First rune is not a BOM, so put it back
+		br.UnreadRune()
+	}
+
+	return csv.NewReader(br)
 }
 
 // fieldsToRecord creates key/value pairs from column names and row values at
--- main_test.go
+++ main_test.go
@@ -2,59 +2,246 @@ package main
 
 import (
 	"bytes"
+	"encoding/csv"
+	"github.com/integrii/flaggy"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"testing"
 )
 
-func TestCsv2Json(t *testing.T) {
+func TestCli(t *testing.T) {
 	for _, tt := range []struct {
-		testName     string
-		forceColumns []string
-		csv          string
-		wantJson     string
+		testName      string
+		useNamedInput bool
+		useTempFile   bool
+		csv           string
+		wantJsonOut   string
+		wantErr       error
+		cliArgs       []string
+	}{
+		{
+			"Basic conversion from named input",
+			true,
+			true,
+			"a,b,c\n1,2,3\nz,y,x\n",
+			`[{"a": "1", "b": "2", "c": "3"}, {"a": "z", "b": "y", "c": "x"}]`,
+			nil,
+			[]string{},
+		},
+		{
+			"Basic conversion from stdin",
+			false,
+			true,
+			"a,b,c\n1,2,3\nz,y,x\n",
+			`[{"a": "1", "b": "2", "c": "3"}, {"a": "z", "b": "y", "c": "x"}]`,
+			nil,
+			[]string{},
+		},
+		{
+			"Fails when missing file is named",
+			true,
+			false,
+			"a,b,c\n1,2,3\nz,y,x\n",
+			"",
+			os.ErrNotExist,
+			[]string{},
+		},
+		{
+			"Fails when CSV field count",
+			true,
+			true,
+			"a,b,c\n1,2\nz,y,x\n",
+			"",
+			csv.ErrFieldCount,
+			[]string{},
+		},
+		{
+			"Cannot skip CSV parse error in header line",
+			true,
+			true,
+			"a,\"b,c\n1,2,3\nz,y,x\n",
+			"",
+			csv.ErrQuote,
+			[]string{"--skip-errors"},
+		},
+		{
+			"Can skip CSV parse error in non-header line",
+			true,
+			true,
+			"a,b,c\n1,\"\"2',3\nz,y,x\n",
+			`[{"a": "z", "b": "y", "c": "x"}]`,
+			nil,
+			[]string{"--skip-errors"},
+		},
+	} {
+		t.Run(tt.testName, func(t *testing.T) {
+
+			tempFile, err := ioutil.TempFile("", "csv2json-test-*")
+			require.NoError(t, err, "Test cannot run without a temp file")
+
+			if tt.useTempFile {
+				_, err = tempFile.WriteString(tt.csv)
+				require.NoError(t, err, "Test cannot run without populated CSV")
+				_, err = tempFile.Seek(0, 0)
+				require.NoError(t, err, "Test cannot run without CSV ready for reading")
+				require.FileExists(t, tempFile.Name(), "Test cannot run without a temp file")
+				t.Cleanup(func() {
+					if err := os.Remove(tempFile.Name()); err != nil {
+						t.Fatalf("Error removing tempfile during cleanup")
+					}
+				})
+			} else {
+				if err := os.Remove(tempFile.Name()); err != nil {
+					t.Fatalf("Error removing tempfile during setup")
+				}
+			}
+
+			cliArgs := append([]string{"csv2json"}, tt.cliArgs...)
+			if tt.useNamedInput {
+				// Provide the CSV temp file as positional CLI argument
+				cliArgs = append(cliArgs, tempFile.Name())
+			} else {
+				// Substitute CSV temp file for stdin
+				oldStdIn := os.Stdin
+				os.Stdin = tempFile
+				t.Cleanup(func() {
+					// Restore stdin
+					os.Stdin = oldStdIn
+				})
+			}
+			os.Args = cliArgs
+			flaggy.ResetParser()
+
+			// Capture stdout for JSON assertions and suppress logging to stderr
+			capturedStdout, runErr := func() (string, error) {
+				oldStdout := os.Stdout
+				oldLogOutput := log.Writer()
+				defer func() {
+					// Restore stdout and resume logging to stderr
+					os.Stdout = oldStdout
+					log.SetOutput(oldLogOutput)
+				}()
+				stdoutReader, stdoutWriter, _ := os.Pipe()
+				os.Stdout = stdoutWriter
+				log.SetOutput(bytes.NewBuffer([]byte{})) // Discard logs sent to stderr
+
+				runErr := runCli()
+
+				stdoutWriter.Close()
+				var buf bytes.Buffer
+				io.Copy(&buf, stdoutReader)
+				return buf.String(), runErr
+			}()
+
+			if tt.wantErr == nil {
+				assert.NoError(t, runErr)
+				assert.JSONEq(t, tt.wantJsonOut, capturedStdout)
+			} else {
+				assert.ErrorIs(t, runErr, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCsv2Json(t *testing.T) {
+	// Log errors to nowhere while this test runs
+	oldLogOutput := log.Writer()
+	log.SetOutput(bytes.NewBuffer([]byte{}))
+	t.Cleanup(func() {
+		log.SetOutput(oldLogOutput)
+	})
+
+	for _, tt := range []struct {
+		testName      string
+		forceColumns  []string
+		csv           string
+		wantJson      string
+		skipErrors    bool
+		expectedError error
 	}{
 		{
 			"Basic conversion with columns from first row",
 			[]string{},
 			"a,b,c\n1,2,3\nz,y,x\n",
 			`[{"a": "1", "b": "2", "c": "3"}, {"a": "z", "b": "y", "c": "x"}]`,
+			false,
+			nil,
 		},
 		{
 			"Whitespace CSV converts to empty JSON array",
 			[]string{},
 			"    ",
 			`[]`,
+			false,
+			nil,
 		},
 		{
 			"Empty CSV converts to empty JSON array",
 			[]string{},
 			"",
 			`[]`,
+			false,
+			nil,
 		},
 		{
 			"Empty CSV with forced columns converts to empty JSON array",
 			[]string{"a", "b", "c"},
 			"",
 			`[]`,
+			false,
+			nil,
 		},
 		{
 			"Basic conversion with forced columns",
 			[]string{"a", "b", "c"},
 			"alpha,bravo,charlie\n1,2,3\nz,y,x\n",
 			`[{"a": "alpha", "b": "bravo", "c": "charlie"}, {"a": "1", "b": "2", "c": "3"}, {"a": "z", "b": "y", "c": "x"}]`,
+			false,
+			nil,
+		},
+		{
+			"Errors abort conversion",
+			[]string{},
+			"a,b,c\n1,2,3\nbad,line\nz,y,x\n",
+			`[{"a": "1", "b": "2", "c": "3"}, {"a": "z", "b": "y", "c": "x"}]`,
+			false,
+			&csv.ParseError{
+				StartLine: 3,
+				Line:      3,
+				Column:    0,
+				Err:       csv.ErrFieldCount,
+			},
+		},
+		{
+			"Errors can be skipped",
+			[]string{},
+			"a,b,c\n1,2,3\nbad,line\nz,y,x\n",
+			`[{"a": "1", "b": "2", "c": "3"}, {"a": "z", "b": "y", "c": "x"}]`,
+			true,
+			nil,
 		},
 	} {
 		t.Run(tt.testName, func(t *testing.T) {
-			csvReader := bytes.NewReader([]byte(tt.csv))
-			jsonWriter := bytes.NewBuffer([]byte{})
+			jsonStream := bytes.NewBuffer([]byte{})
+			options := conversionOptions{
+				colNames:   tt.forceColumns,
+				csvInput:   bytes.NewReader([]byte(tt.csv)),
+				jsonOutput: jsonStream,
+				skipErrors: tt.skipErrors,
+			}
 
-			err := csv2Json(tt.forceColumns, csvReader, jsonWriter)
+			err := csv2Json(options)
 
-			assert.NoError(t, err)
-			assert.JSONEq(t, tt.wantJson, jsonWriter.String())
+			if tt.expectedError == nil {
+				assert.NoError(t, err)
+				assert.JSONEq(t, tt.wantJson, jsonStream.String())
+			} else if !tt.skipErrors {
+				assert.EqualError(t, err, tt.expectedError.Error())
+			}
 		})
 	}
 }
